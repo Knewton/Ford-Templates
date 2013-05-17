20b60d182df24299b745e9ab0e3c6936a0b64934
Eric Garside <eric@knewton.com>
2013-05-17 10:59:06 -0400

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/*jslint regexp: true, browser: true, maxerr: 50, indent: 4, maxlen: 79 */
+(function () {
+	"use strict";
+
+	Screw.Unit(function () {
+		describe("The system", function () {
+			it("can pass tests", function () {
+				expect(false).to(be_true);
+			});
+		});
+	});
+
+}());
+
